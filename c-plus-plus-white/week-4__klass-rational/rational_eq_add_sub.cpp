7e77a433bb29ce039daa3f9358aa8ee7f4b87c27
Nadezhda Ryabtsova <nadezhdaryabtsova@gmail.com>
2021-10-09 04:15:10 +0300

    .

---
+++ c-plus-plus-white/week-4__klass-rational/rational_eq_add_sub.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+// один из способов вычисления наибольшего общего делителя (НОД) — рекурсивный:
+// вместо цикла функция будет вызывать себя же, но с другими аргументами
+int GreatestCommonDivisor(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return GreatestCommonDivisor(b, a % b);
+    }
+}
+
+class Rational {
+public:
+    Rational() {  // дробь по умолчанию — 0/1
+        numerator = 0;
+        denominator = 1;
+    }
+    Rational(int new_numerator, int new_denominator) {
+        const int gcd = GreatestCommonDivisor(new_numerator, new_denominator);
+        // сократим дробь, разделив числитель и знаменатель на их НОД
+        numerator = new_numerator / gcd;
+        denominator = new_denominator / gcd;
+        // знаменатель должен быть положительным
+        if (denominator < 0) {
+            denominator = -denominator;
+            numerator = -numerator;
+        }
+    }
+
+    int Numerator() const {
+        return numerator;
+    }
+
+    int Denominator() const {
+        return denominator;
+    }
+
+private:
+    int numerator;
+    int denominator;
+};
+
+// поскольку дроби сокращены, достаточно сравнить числители и знаменатели
+bool operator == (const Rational& lhs, const Rational& rhs) {
+    return lhs.Numerator() == rhs.Numerator() &&
+           lhs.Denominator() == rhs.Denominator();
+}
+
+// используем обычную формулу сложения дробей, основанную на приведении слагаемых к общему знаменателю
+Rational operator + (const Rational& lhs, const Rational& rhs) {
+    return {
+            lhs.Numerator() * rhs.Denominator() + rhs.Numerator() * lhs.Denominator(),
+            lhs.Denominator() * rhs.Denominator()
+    };
+}
+
+// вычитание реализуем аналогично сложению
+// дублирования кода можно было избежать, определив для класса Rational операцию унарного минуса: тогда разность lhs и rhs можно было бы вычислить           как lhs + (-rhs)
+Rational operator - (const Rational& lhs, const Rational& rhs) {
+    return {
+            lhs.Numerator() * rhs.Denominator() - rhs.Numerator() * lhs.Denominator(),
+            lhs.Denominator() * rhs.Denominator()
+    };
+}
+
+int main() {
+    {
+        Rational r1(4, 6);
+        Rational r2(2, 3);
+        bool equal = r1 == r2;
+        if (!equal) {
+            cout << "4/6 != 2/3" << endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a + b;
+        bool equal = c == Rational(2, 1);
+        if (!equal) {
+            cout << "2/3 + 4/3 != 2" << endl;
+            return 2;
+        }
+    }
+
+    {
+        Rational a(5, 7);
+        Rational b(2, 9);
+        Rational c = a - b;
+        bool equal = c == Rational(31, 63);
+        if (!equal) {
+            cout << "5/7 - 2/9 != 31/63" << endl;
+            return 3;
+        }
+    }
+
+    cout << "OK" << endl;
+    return 0;
+}
