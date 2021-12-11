6f2de5ba7b7ba54e9b6527cecf6ae6b7b1636254
Nadezhda Ryabtsova <nadezhdaryabtsova@gmail.com>
2021-12-11 20:24:37 +0300

    .

---
+++ c-plus-plus-yellow/week-1__baza-rieghionov/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Напишите функцию FindMaxRepetitionCount, принимающую базу регионов и определяющую,
+ * какое максимальное количество повторов (число вхождений одного и того же элемента) она содержит.
+ * Две записи (объекты типа Region) считаются различными, если они отличаются хотя бы одним полем.
+ */
+
+#include <map>
+#include <tuple>
+#include <vector>
+
+using namespace std;
+
+bool operator<(const Region& lhs, const Region& rhs) {
+    return tie(lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
+           tie(rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
+}
+
+int FindMaxRepetitionCount(const vector<Region>& regions) {
+    int result = 0;
+    map<Region, int> repetition_count;
+    for (const Region& region : regions) {
+        result = max(result, ++repetition_count[region]);
+    }
+    return result;
+}
